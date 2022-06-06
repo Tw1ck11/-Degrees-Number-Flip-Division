8fa02565d82ec80b1fe52b0f04b1b5d82379b9ac
Tw1ck11 <qwentyn2222222222@gmail.com>
2022-06-06 22:40:20 +0200

    Degrees Number Flip Division

---
+++ Градусы Переворот числа Деление.cpp
@@ -0,0 +1,87 @@
+﻿// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	////////Task1
+	int start;
+	int end;
+
+	cin >> start;
+	cin >> end;
+	cout << "C          F" << endl;
+	while (start <= end) {
+		cout << start << "          " << start * 1.8 + 32 << endl;
+		start++;
+	}
+	cout << "" << endl;
+
+
+
+
+
+
+
+	////////Task2
+	{
+		int num;
+		int tmp;
+		int reversed_num = 0;
+
+		cout << "Enter a number: ";
+		cin >> num;
+
+		while (num != 0)
+		{
+			tmp = num % 10;
+			reversed_num = reversed_num * 10 + tmp;
+			num /= 10;
+		}
+
+		cout << "Reversed number: " << reversed_num << "\n";
+	}
+
+
+
+
+	///////TASK 3
+	int number, k = 0;
+	cin >> number;
+	while (k < number - 1) {
+		k++;
+		if (number % k == 0) {
+			cout << k << ", ";
+		}
+		else {
+			cout << "";
+		}
+	}
+	cout << number << "." << endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
